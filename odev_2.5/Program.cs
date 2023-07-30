bd900e75bca0092790e1f3de4a3f06d634a9d7fb
lesponns <adamadakadam1903@gmail.com>
2023-07-30 06:19:09 +0300

    Add project files.

---
+++ odev_2.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Başlangıç: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.Write("Bitiş: ");
+            int end = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            if (start < end) {
+                for (int i = start; i <= end; i++) {
+                    if (i % 5 == 0) {
+                        Console.Write(i + " \n");
+                    }
+                }
+            }
+            else { Console.WriteLine("Errcode.Fatal[The beginning cannot be greater than the end.]"); }
+
+            Console.ReadKey();
+        }
+    }
+}
